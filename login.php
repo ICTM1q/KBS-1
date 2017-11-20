44a097d7e45ccd374684903e936c4e55ae7dd3e1
Eterniity <remcostoelwinder@hotmail.com>
2017-11-20 12:10:06 +0100

    File Structuur
    Structuur toegepast aan de bestanden zodat het niet een zootje is.

--- login.php
+++ login.php
@@ -81,7 +81,7 @@
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" crossorigin="anonymous">
-            <link rel="stylesheet" href="login.css">
+            <link rel="stylesheet" href="css/login.css">
         </head>
         <body>
             <div id="body"></div>
