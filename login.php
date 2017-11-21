7a4c3e6d0a029903a3eff893e9b740a2f8683338
Erik <s1117904@student.windesheim.nl>
2017-11-21 10:06:56 +0100

    Login page update
    Dit is de update van de login page.

--- login.php
+++ login.php
@@ -88,67 +88,37 @@
             <div id="lijnwit">
             <div id="lijnblauw">
             <div id="header">
-                <div id="navbar">
-                    <nav class="navbar navbar-expand-lg">
-                        <a class="navbar-brand" href="home.php">LOGO</a>
-                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-                                <span class="navbar-toggler-icon"></span>
-                            </button>
-
-                        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-                            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                                <li class="nav-item">
-                                    <a class="nav-link" href="home.php">Home</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="kantoor.php">Kantoor</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="aanbod.php">Aanbod</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="huurvoorwaarden.php">Huurvoorwaarden</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="inschrijven.php">Inschrijven</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="beheer.php">Beheer</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="contact.php">Contact</a>
-                                </li>
-                            </ul>
-                            <a class="navbar-brand" href="home.php">LOGO</a>
-                        </div>
-                    </nav>      
-                </div>
                 <div id="tekst">
                     <h1 id="adminh1">Alleen geautoriseerde toegang!</h1>
-                    <form method="POST">
-                        Gebruikersnaam:<br>
-                        <input type="text" name="loginUsername" value="<?php if ( isset ( $_POST["loginUsername"] ) ) { print $_POST["loginUsername"]; } ?>"
-                        <span class="error"><?php echo $loginArray["usernameErr"];?></span><br>
-                        Wachtwoord:<br>
-                        <input type="password" name="loginPassword">
-                        <span class="error"><?php echo $loginArray["passwordErr"];?></span>
-                        <!-- Als iemand drie pogingen heeft gedaan krijgt hij/zij een captcha. De PHP code hieronder zorgt ervoor dat de captcha in beeld komt. -->
+                    <div id="forms">
+                        <form method="post">
+                            Gebruikersnaam:<br>
+                            <input type="text" name="loginUsername" value="<?php if ( isset ( $_POST["loginUsername"] ) ) { echo $_POST["loginUsername"]; } ?>">
+                            <span class="error"><?php echo $loginArray["usernameErr"];?></span><br>
+                            Wachtwoord:<br>
+                            <input type="password" name="loginPassword">    
+                            <span class="error"><?php echo $loginArray["passwordErr"];?></span>
+                            <span class="error"><?php echo $loginArray["result"];?></span><br>
+                            <a class="ondertekst" href="home.php">[ Wachtwoord vergeten ]<br></a>
+                            <!-- Als iemand drie pogingen heeft gedaan krijgt hij/zij een captcha. De PHP code hieronder zorgt ervoor dat de captcha in beeld komt. -->
+                    </div>
+                    <div id="captcha">
                         <?php 
                         if ( $tries >= 3 ) { 
-                            echo "<br><img id='captcha' src='lib/securimage/securimage_show.php' alt='CAPTCHA Image' }; /> <br>";
-                            echo "<a href='' onclick='document.getElementById('captcha').src = 'lib/securimage/securimage_show.php?' + Math.random(); return false'>[ Andere Afbeelding ]</a> <br>";
+                            echo "<br><img id='captcha' class='img' src='lib/securimage/securimage_show.php' alt='CAPTCHA Image' }; /> <br>";
+                            echo "<a class='ondertekst' href='' onclick='document.getElementById('captcha').src = 'lib/securimage/securimage_show.php?' + Math.random(); return false'>[ Andere Afbeelding ]</a> <br>";
                             echo "<input type='text' name='captcha_code' size='10' maxlength='6' />";      
                         }
                         ?>
+                    
                         <span class="error"><?php echo $loginArray["captchaErr"];?></span><br>
-                        <a id="wachtwoord" href="home.php">Wachtwoord vergeten <br></a>
-                        <span class="error"><?php echo $loginArray["result"];?></span><br>
+                    </div>    
+                        
                         <input type="submit" value="Verstuur" name="submit" id="knop">
                     </form>
                 </div>
                 </div>
             </div>
             </div>
-            <div id="bot"></div>
         </body>
     </html>
