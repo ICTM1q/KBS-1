2fe1d90569fe55ed3e19bf5ef842a7ab2bcfac93
Erik <s1117904@student.windesheim.nl>
2017-11-28 16:06:37 +0100

    Home pagina van huur en beheer

---
+++ huurenbeheer/home.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Huur en beheer</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link href="css/huurenbeheer.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-custom fixed-top">
+     <div class="container">
+       <a class="navbar-brand" href="index.php">
+           <img src="css/img/logo.jpg" width="160" height="90" class="d-inline-block align-top" alt=""></a>
+       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+         <span class="navbar-toggler-icon"></span>
+       </button>
+       <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="home.php">Home
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="kantoor.php">Kantoor</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Aanbod</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Huurvoorwaarden</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Inschrijven</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Formulieren</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Beheer</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container-white">
+
+      <!-- Heading Row -->
+      <div class="row my-4">
+        <div class="col-lg-8">
+            <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner sliderbox">
+                  <div class="carousel-item active">
+                    <img class="d-block w-100" src="css/img/slider-01.jpg" alt="First slide">
+                  </div>
+                  <div class="carousel-item">
+                    <img class="d-block w-100" src="css/img/slider-02.jpg" alt="Second slide">
+                  </div>
+                  <div class="carousel-item">
+                    <img class="d-block w-100" src="css/img/slider-03.jpg" alt="Third slide">
+                  </div>
+                </div>
+              </div>
+        </div>
+        <!-- /.col-lg-8 -->
+        <div class="col-lg-4">
+            <h1 class="tagline">Welkom op de website van Huur & Beheer Hoksbergen</h1>
+          <p>U bent bij ons op het juiste adres voor:</p>
+          <ul>
+              <li>het bemiddelen bij de verhuur van uw woning of bedrijfspand.</li>
+              <li>het zoeken naar een huurwoning / bedrijfspand.</li>
+              <li>het uitbesteden van beheer van uw particulier of bedrijfsmatig vastgoed.</li>
+          </ul>
+          <p>Bel of mail ons voor een persoonlijk gesprek!</p>
+        </div>
+        <!-- /.col-md-4 -->
+        <!--<div class="cert1"><img src="css/img/cert1.jpg"></div>
+        <div class="cert2"><img src="css/img/cert2.jpg"></div>-->
+      </div>
+      <!-- /.row -->
+
+      <!-- Call to Action Well -->
+      <div class="card text-white bg-secondary my-4 text-center">
+        <div class="card-body">
+          <p class="text-white m-0">This call to action card is a great place to showcase some important information or display a clever tagline!</p>
+        </div>
+      </div>
+
+      <!-- Content Row -->
+      <div class="row">
+        <div class="col-md-4 mb-4">
+          <div class="card h-100">
+            <div class="card-body">
+              <h2 class="card-title">Card One</h2>
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem magni quas ex numquam, maxime minus quam molestias corporis quod, ea minima accusamus.</p>
+            </div>
+            <div class="card-footer">
+              <a href="#" class="btn btn-primary">More Info</a>
+            </div>
+          </div>
+        </div>
+        <!-- /.col-md-4 -->
+        <div class="col-md-4 mb-4">
+          <div class="card h-100">
+            <div class="card-body">
+              <h2 class="card-title">Card Two</h2>
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod tenetur ex natus at dolorem enim! Nesciunt pariatur voluptatem sunt quam eaque, vel, non in id dolore voluptates quos eligendi labore.</p>
+            </div>
+            <div class="card-footer">
+              <a href="#" class="btn btn-primary">More Info</a>
+            </div>
+          </div>
+        </div>
+        <!-- /.col-md-4 -->
+        <div class="col-md-4 mb-4">
+          <div class="card h-100">
+            <div class="card-body">
+              <h2 class="card-title">Card Three</h2>
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem magni quas ex numquam, maxime minus quam molestias corporis quod, ea minima accusamus.</p>
+            </div>
+            <div class="card-footer">
+              <a href="#" class="btn btn-primary">More Info</a>
+            </div>
+          </div>
+        </div>
+        <!-- /.col-md-4 -->
+
+      </div>
+      <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+
+   <footer class="py-5 footer-custom">
+     <div class="container">
+       <div class="row ">
+          <div class="col vastgoed-img">
+              <h3>Certificaten</h3>
+                <a href="https://www.vastgoedpro.nl/"><img src="css/img/vgp.jpg" alt=""></a>
+                <br>
+                <br>
+                <a href="https://www.vastgoedcert.nl/"><img src="css/img/vastgoedcert.jpg" alt=""></a>
+                <br>
+                <br>
+            </div>
+            <div class="col">
+              <h3>KVK/BTW</h3>
+              <ul class="ulfooter">
+                <li>KvK: 05059210</li>
+                <li>BTW nr.: 810305628</li>
+              </ul>
+            </div>
+            <div class="col">
+              <h3>Adres</h3>
+                  <ul class="ulfooter"><li>Oudestraat 172</li>
+                  <li>8261 CW Kampen</li>
+                  <li>Tel.: 038-33 88 341</li>
+                  <li>info@hoksbergen.nl</li></ul>
+            </div>
+            <div class="col">
+              <h3>Locatie</h3>
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2425.5411569166!2d5.9132473156257745!3d52.559824241029794!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c87883431521b5%3A0x9589572588a7b0f1!2sHoksbergen+Makelaardij+V.O.F.!5e0!3m2!1snl!2snl!4v1511426183418" width="400" height="200" frameborder="0" style="border:0" allowfullscreen></iframe>
+            </div>
+          </div>
+          <br>
+         <div class="row">
+           <div class="col-sm-12 col-md-12 text-center">
+             <span>Hoksbergen Makelaardij &copy; 2017 </span>
+             <div class="blank-gap-10"></div>
+             <p><p>
+             </div>
+         </div>
+    </div>
+   </footer>
+
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+    <script>
+        $('.carousel').carousel({
+            interval: 5000
+        })
+    </script>
+  </body>
+</html>
