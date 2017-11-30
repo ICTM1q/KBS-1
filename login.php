86849d37d39e79025273e20cbde05715c6057d01
Tristan Willems <tristan_j_w@hotmail.com>
2017-11-30 14:17:48 +0100

    Made changes to paths and added errorreporting to login.

--- login.php
+++ login.php
@@ -84,6 +84,7 @@
         </head>
         <body>
                 <div class="tekst">
+                    <?php include "admin-components/alert.php" ?>
                     <h1 class="adminh1">Alleen geautoriseerde toegang!</h1>
                     <div class="forms">
                         <form method="post">
