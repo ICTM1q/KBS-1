fc77758ebea65e49f61880bfd887ed3e6465e7c8
Tristan Willems <tristan_j_w@hotmail.com>
2017-12-06 10:38:23 +0100

    Modified logo in back-end

--- admin/header.php
+++ admin/header.php
@@ -31,7 +31,7 @@ header( "Location: /login.php" );
 <body>
 <header>
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <a class="navbar-brand navbar-brand-custom" href="/admin.php"><img src="/images/logo-cropped.jpg"></a>
+        <a class="navbar-brand navbar-brand-custom" href="/admin.php"><img src="/images/logo_groot.png"></a>
         <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
