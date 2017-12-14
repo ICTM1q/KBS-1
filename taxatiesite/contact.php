79dfdbcbf92c6dde10848c6a756856944429e152
Geert <geert.zwakenberg@live.nl>
2017-12-14 13:32:29 +0100

    taxatie pdf

--- taxatiesite/contact.php
+++ taxatiesite/contact.php
@@ -1,3 +1,46 @@
+<?php
+session_start();
+include "pdf-taxatie.php";
+require $_SERVER['DOCUMENT_ROOT']."../lib/mail/mail.php";
+
+// Definier captchaError leeg.
+$pdfHTContactArray["firstnameErr"] = "";
+$pdfHTContactArray["lastnameErr"] = "";
+$pdfHTContactArray["emailErr"] = "";
+$pdfHTContactArray["telnoErr"] = "";
+$pdfHTContactArray["messageErr"] = "";
+$pdfHTContactArray["captchaErr"] = "";
+$pdfHTContactArray["result"] = "";
+$pdfHTContactArray["message"] = "";
+
+// Als het knopje ingedrukt is.
+if ( isset( $_POST["submit"] ) ){ 
+    include_once "lib/securimage/securimage.php";
+    $secureImage = new Securimage();
+    
+    // Voer pdfFunc uit.
+    $pdfHTContactArray = pdfHTContactFunc($_POST["firstname"], $_POST["insertion"], $_POST["surname"], $_POST["email"], $_POST["telno"], $_POST["street"], $_POST["city"], $_POST["houseno"], $_POST["zip"], $_POST["message"], $secureImage, $_POST["captchaCode"]);
+    if ( $pdfHTContactArray["result"] === TRUE ) {
+        // Voor testing wanneer je geen mail win ontvangen zet comments bij sendContactMail en geen comments bij de ->Output() functie.
+        $pdfHTContactArray["pdf"]->Output();
+        /*if ( sendContactMail ( $pdfHTContactArray["pdf"]->Output("contactformulier.pdf", 'S'), "Contact", $_POST["firstname"], $_POST["surname"] ) ) {
+            $pdfHTContactArray["success"] = TRUE;
+            $pdfHTContactArray["message"] = "Wij hebben uw contactverzoek ontvangen en zullen hem zo spoedig mogelijk afhandelen!";
+        }
+        else {
+            $pdfHTContactArray["success"] = FALSE;
+            $pdfHTContactArray["message"] = "Er is een probleem opgetreden, probeer het later nogmaals.";
+        }*/
+    }
+    else {
+        $pdfHTContactArray["success"] = FALSE;
+        $pdfHTContactArray["message"] = "Er zijn errors opgetreden, los deze op en probeer het nogmaals.";
+    }
+}
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -64,6 +107,10 @@
                          <div class="form-group">
                              <label for="form_name">Voornaam*</label>
                              <input id="form_name" type="text" name="name" class="form-control" placeholder="Voornaam" required="required" data-error="Voornaam is verplicht.">
+                             <?php
+                             if ( !empty($pdfHTContactArray["firstnameErr"])) {
+                                echo "<span class='error'>" . $pdfHTContactArray["firstnameErr"] . "</span><br>";
+                             ?>
                              <div class="help-block with-errors"></div>
                          </div>
                      </div>
@@ -71,6 +118,11 @@
                          <div class="form-group">
                              <label for="form_lastname">Achternaam*</label>
                              <input id="form_lastname" type="text" name="surname" class="form-control" placeholder="Achternaam" required="required" data-error="Achternaam is verplicht">
+                             <?php
+                              if ( !empty($pdfHTContactArray["surnameErr"])) {
+                                echo "<span class='error'>" . $pdfHTContactArray["surnameErr"] . "</span><br>";
+                              }
+                              ?>
                              <div class="help-block with-errors"></div>
                          </div>
                      </div>
@@ -80,6 +132,11 @@
                          <div class="form-group">
                              <label for="form_email">Email adres*</label>
                              <input id="form_email" type="email" name="email" class="form-control" placeholder="Email adres" required="required" data-error="Een geldige email is verplicht">
+                             <?php
+                              if ( !empty($pdfHTContactArray["emailErr"])) {
+                                echo "<span class='error'>" . $pdfHTContactArray["emailErr"] . "</span><br>";
+                              }
+                              ?>
                              <div class="help-block with-errors"></div>
                          </div>
                      </div>
@@ -87,6 +144,11 @@
                          <div class="form-group">
                              <label for="form_phone">Telefoonnummer*</label>
                              <input id="form_phone" type="tel" name="phone" class="form-control" placeholder="Telefoonnummer">
+                             <?php
+                              if ( !empty($pdfHTContactArray["telnoErr"])) {
+                                echo "<span class='error'>" . $pdfHTContactArray["telnoErr"] . "</span><br>";
+                              }
+                              ?>
                              <div class="help-block with-errors"></div>
                          </div>
                      </div>
@@ -95,18 +157,40 @@
                    <div class="form-group col-md-6">
                      <label for="inputCity">Straatnaam*</label>
                      <input type="text" class="form-control" id="inputCity" placeholder="Straatnaam">
+                   <?php
+                      if ( !empty($pdfHTContactArray["streetErr"])) {
+                        echo "<span class='error'>" . $pdfHTContactArray["streetErr"] . "</span><br>";
+                      }
+                      ?>
                    </div>
+                     
                     <div class="form-group col-md-6">
                       <label for="inputCity">Plaatsnaam*</label>
                       <input type="text" class="form-control" id="inputCity" placeholder="Plaatsnaam">
+                    <?php
+                       if ( !empty($pdfHTContactArray["cityErr"])) {
+                        echo "<span class='error'>" . $pdfHTContactArray["cityErr"] . "</span><br>";
+                       }
+                       ?>
                     </div>
+                     
                     <div class="form-group col-md-6">
                       <label for="inputZip">Huisnummer*</label>
                       <input type="text" class="form-control" id="inputZip" placeholder="Huisnummer">
+                    <?php
+                       if ( !empty($pdfHTContactArray["housenoErr"])) {
+                         echo "<span class='error'>" . $pdfHTContactArray["housenoErr"] . "</span><br>";
+                       }
+                       ?>
                     </div>
                     <div class="form-group col-md-6">
                       <label for="inputZip">Postcode*</label>
                       <input type="text" class="form-control" id="inputZip" placeholder="Postcode">
+                     <?php
+                       if ( !empty($pdfHTContactArray["zipErr"])) {
+                         echo "<span class='error'>" . $pdfHTContactArray["zipErr"] . "</span><br>";
+                       }
+                       ?>
                     </div>
                   </div>
                  <div class="row">
@@ -114,12 +198,19 @@
                          <div class="form-group">
                              <label for="form_message">Bericht*</label>
                              <textarea id="form_message" name="message" class="form-control" placeholder="Bericht" rows="4" required="required" data-error="Laat in dit veld een bericht achter."></textarea>
+                             <?php
+                              if ( !empty($pdfHTContactArray["messageErr"])) {
+                                echo "<span class='error'>" . $pdfHTContactArray["messageErr"] . "</span><br>";
+                              }
+                              ?>
                              <div class="help-block with-errors"></div>
                          </div>
                      </div>
                      <div class="col-md-12">
-                       Captacha hier
-                     </div>
+                        <img id="captcha" src="../lib/securimage/securimage_show.php" alt="CAPTCHA Image" /><br>
+                        <a href="#" onclick="document.getElementById('captcha').src = '../lib/securimage/securimage_show.php?' + Math.random(); return false">[ Andere Afbeelding ]</a><br>
+                        <input type="text" name="captchaCode" size="10" maxlength="6" /><span class="error">  <?php echo $pdfHTContactArray["captchaErr"]; ?><span><br><br>
+                      </div>
                      <br>
                      <br>
                      <div class="col-md-12">
@@ -130,7 +221,6 @@
                      <div class="col-md-12">
                        <br>
                          <p class="text-muted">Velden met een * zijn verplicht. </p>
-                         <p>nieuw</p>
                      </div>
                  </div>
              </div>
---
+++ taxatiesite/pdf-taxatie.php
@@ -0,0 +1,357 @@
+<!DOCTYPE HTml>
+
+<HTml>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+      
+require ("../lib/fpdf/fpdf.php");
+
+// Funcite om contactverzoeken af te handelen.
+function pdfHTContactFunc ( $firstname, $insertion, $surname, $email, $telno, $street, $city, $houseno, $zip, $message, $secureImage, $captchaCode ) {
+    // Alles definieren.
+    $pdfHTContactArray = array();
+    $pdfHTContactArray["result"] = FALSE;
+    
+    $pdfHTContactFirstnameFlag = TRUE;
+    $pdfHTContactSurnameFlag = TRUE;
+    $pdfHTContactEmailFlag = TRUE;
+    $pdfHTContactTelnoFlag = TRUE;
+    $pdfHTContactStreetFlag = TRUE;
+    $pdfHTContactCityFlag = TRUE;
+    $pdfHTContactHousenoFlag = TRUE;
+    $pdfHTContactZipFlag = TRUE;
+    $pdfHTContactMessageFlag = TRUE;
+    $pdfHTContactCaptchaFlag = TRUE;
+    
+    $pdfHTContactArray["firstnameErr"] = "";
+    $pdfHTContactArray["surenameErr"] = "";
+    $pdfHTContactArray["emailErr"] = "";
+    $pdfHTContactArray["telnoErr"] = "";
+    $pdfHTContactArray["streetErr"] = "";
+    $pdfHTContactArray["cityErr"] = "";
+    $pdfHTContactArray["housenoErr"] = "";
+    $pdfHTContactArray["zipErr"] = "";
+    $pdfHTContactArray["messageErr"] = "";
+    $pdfHTContactArray["captchaErr"] = "";
+    
+    // Kijk of email leeg is.
+    if ( empty ( $firstname ) ) {
+        $pdfHTContactArray["firstnameErr"] = "Voornaam is vereist.";
+        $pdfHTContactFirstnameFlag = FALSE;
+    }
+    if ( empty ( $surname ) ) {
+        $pdfHTContactArray["surnameErr"] = "AcfHTernaam is vereist.";
+        $pdfHTContactSurnameFlag = FALSE;
+    }
+    if ( empty ( $email ) ) {
+        $pdfHTContactArray["emailErr"] = "Email is vereist.";
+        $pdfHTContactEmailFlag = FALSE;
+    }
+    if ( empty ( $telno ) ) {
+        $pdfHTContactArray["telnoErr"] = "Telefoonnummer is vereist.";
+        $pdfHTContactTelnoFlag = FALSE;
+    }
+    if ( empty ( $street ) ) {
+        $pdfHTContactArray["streetErr"] = "Straat is vereist.";
+        $pdfHTContactStreetFlag = FALSE;
+    }
+    if ( empty ( $city ) ) {
+        $pdfHTContactArray["cityErr"] = "Plaats is vereist.";
+        $pdfHTContactCityFlag = FALSE;
+    }
+    if ( empty ( $houseno ) ) {
+        $pdfHTContactArray["housenoErr"] = "Huisnummer is vereist.";
+        $pdfHTContactHousenoFlag = FALSE;
+    }
+    if ( empty ( $zip ) ) {
+        $pdfHTContactArray["zipErr"] = "Postcode is vereist.";
+        $pdfHTContactZipFlag = FALSE;
+    }
+    // Kijk of message leeg is.
+    if ( empty ( $message ) ) {
+        $pdfHTContactArray["messageErr"] = "Voer A.U.B. uw bericfHT in.";
+        $pdfHTContactMessageFlag = FALSE;
+    }
+    // Kijk of captchacode leeg is.
+    if ( empty ( $captchaCode ) ) {
+        $pdfHTContactArray["captchaErr"] = "Captcha is vereist.";
+        $pdfHTContactCaptchaFlag = FALSE;
+    }
+    // Kijk of alles nog op TRUE staat.
+    if ( $pdfHTContactFirstnameFlag === TRUE && $pdfHTContactSurnameFlag === TRUE && $pdfHTContactEmailFlag == TRUE && $pdfHTContactTelnoFlag == TRUE 
+            && $pdfHTContactStreetFlag === TRUE && $pdfHTContactCityFlag = TRUE && $pdfHTContactMessageFlag == TRUE && $pdfHTContactCaptchaFlag == TRUE 
+            && $pdfHTContactHousenoFlag === TRUE && $pdfHTContactZipFlag === TRUE ) {
+        
+        // Zet flags voor validatie email en telefoonnummer. 
+        $pdfHTContactArrayEmailIsValidFlag = TRUE;
+        $pdfHTContactArrayTelnoIsValidFlag = TRUE;
+        // Kijk of email valid is.
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $pdfHTContactArray["emailErr"] = "Email is ongeldig.";
+            $pdfHTContactArrayEmailIsValidFlag = FALSE;
+        }
+        // Kijk of telefoonnummer alleen cijfers bevat.
+        if (!is_numeric($telno)) {
+            $pdfHTContactArray["telnoErr"] = "Telefoon nummer is ongeldig.";
+            $pdfHTContactArrayTelnoIsValidFlag = FALSE;
+        }
+        // Als alles klopt ga door.
+        if ( $pdfHTContactArrayEmailIsValidFlag === TRUE && $pdfHTContactArrayTelnoIsValidFlag === TRUE ) {
+            // Kijk of captcha fout is.
+            if ($secureImage->check($captchaCode) == FALSE) {
+                $pdfHTContactArray["captchaErr"] = "Captcha is fout!";
+            }
+            // Als alles ingevuld is en de captcha goed is maak de pdf.
+            else {
+                class PDF extends FPDF {
+                    function Header() {
+                    $this->Image("lib/fpdf/logo_groot.png",10,5,65);
+                    $this->SetFont("Arial","B",15);
+                    $this->Cell(80);
+                    $this->Cell(35,18,"Contactformulier",0,0,"C");
+                    $this->Line(20, 35, 210-20, 35);
+                    $this->Ln(30);
+                    }
+                }
+                $pdf = new PDF();
+                $pdf->AliasNbPages();
+                $pdf->SetLeftMargin(10);
+                $pdf->AddPage();
+                
+                $pdf->SetFont("Arial","",10);
+                $pdf->SetLeftMargin(10);
+                
+                $pdf->SetXY(23, 40);
+                $pdf->Cell(35,10,"Voornaam: ");
+                $pdf->SetXY(60, 40);
+                $pdf->Cell(35,10,$firstname);
+                
+                $pdf->SetXY(23, 45);
+                if (empty($insertion)) {
+                    $pdf->Cell(35,10,"AcfHTernaam: ");
+                    $pdf->SetXY(60, 45);
+                    $pdf->Cell(35,10,$surname);
+                }
+                else {
+                    $pdf->Cell(35,10,"AcfHTernaam: ");
+                    $pdf->SetXY(60, 45);
+                    $pdf->Cell(35,10,$surname . ", " . $insertion);
+                }
+                
+                $pdf->SetXY(23, 50);
+                $pdf->Cell(35,10,"Email: ");
+                $pdf->SetXY(60, 50);
+                $pdf->Cell(35,10,$email);
+                
+                $pdf->SetXY(23, 55);
+                $pdf->Cell(35,10,"Telefoonnummer: ");
+                $pdf->SetXY(60, 55);
+                $pdf->Cell(35,10,$telno);
+                
+                $pdf->SetXY(23, 60);
+                $pdf->Cell(35,10,"Woonplaats: ");
+                $pdf->SetXY(60, 60);
+                $pdf->Cell(35,10,$city);
+                
+                $pdf->SetXY(23, 65);
+                $pdf->Cell(35,10,"Adres: ");
+                $pdf->SetXY(60, 65);
+                $pdf->Cell(35,10,$street . " " . $houseno);
+                
+                $pdf->SetXY(23, 70);
+                $pdf->Cell(35,10,"Postcode: ");
+                $pdf->SetXY(60, 70);
+                $pdf->Cell(35,10,$zip);
+                
+                $pdf->SetXY(23, 75);
+                $pdf->Line(20, 85, 210-20, 85);
+                $pdf->SetXY(23, 90);
+                $pdf->Multicell(165,4.5,"BericfHT: \n" . $message);
+                
+                $pdfHTContactArray["pdf"] = $pdf;
+                $pdfHTContactArray["result"] = TRUE;
+            }
+        }
+    }
+    return $pdfHTContactArray;
+}
+
+// Functie om meldingen af te handelen.
+function pdfHTComplaintFunc ( $firstname, $insertion, $surname, $email, $telno, $street, $city, $houseno, $zip, $complaint, $secureImage, $captchaCode ) {
+    // Alles definieren.
+    $pdfHTComplaintArray = array();
+    $pdfHTComplaintArray["result"] = FALSE;
+    
+    $pdfHTComplaintFirstnameFlag = TRUE;
+    $pdfHTComplaintSurnameFlag = TRUE;
+    $pdfHTComplaintEmailFlag = TRUE;
+    $pdfHTComplaintTelnoFlag = TRUE;
+    $pdfHTComplaintTelnoFlag = TRUE;
+    $pdfHTComplaintStreetFlag = TRUE;
+    $pdfHTComplaintCityFlag = TRUE;
+    $pdfHTComplaintHousenoFlag = TRUE;
+    $pdfHTComplaintZipFlag = TRUE;
+    $pdfHTComplaintComplaintFlag = TRUE;
+    $pdfHTComplaintCaptchaFlag = TRUE;
+    
+    $pdfHTComplaintArray["firstnameErr"] = "";
+    $pdfHTComplaintArray["surnameErr"] = "";
+    $pdfHTComplaintArray["emailErr"] = "";
+    $pdfHTComplaintArray["telnoErr"] = "";
+    $pdfHTComplaintArray["streetErr"] = "";
+    $pdfHTComplaintArray["cityErr"] = "";
+    $pdfHTComplaintArray["housenoErr"] = "";
+    $pdfHTComplaintArray["zipErr"] = "";
+    $pdfHTComplaintArray["ComplaintErr"] = "";
+    $pdfHTComplaintArray["captchaErr"] = "";
+    
+    // Kijk of email leeg is.
+    if ( empty ( $firstname ) ) {
+        $pdfHTComplaintArray["firstnameErr"] = "Voornaam is vereist.";
+        $pdfHTComplaintFirstnameFlag = FALSE;
+    }
+    if ( empty ( $surname ) ) {
+        $pdfHTComplaintArray["surnameErr"] = "AcfHTernaam is vereist.";
+        $pdfHTComplaintSurnameFlag = FALSE;
+    }
+    if ( empty ( $email ) ) {
+        $pdfHTComplaintArray["emailErr"] = "Email is vereist.";
+        $pdfHTComplaintEmailFlag = FALSE;
+    }
+    if ( empty ( $telno ) ) {
+        $pdfHTComplaintArray["telnoErr"] = "Telefoonnummer is vereist.";
+        $pdfHTComplaintTelnoFlag = FALSE;
+    }
+    if ( empty ( $street ) ) {
+        $pdfHTComplaintArray["streetErr"] = "Straat is vereist.";
+        $pdfHTComplaintStreetFlag = FALSE;
+    }
+    if ( empty ( $city ) ) {
+        $pdfHTComplaintArray["cityErr"] = "Plaats is vereist.";
+        $pdfHTComplaintCityFlag = FALSE;
+    }
+    if ( empty ( $houseno ) ) {
+        $pdfHTComplaintArray["housenoErr"] = "Huisnummer is vereist.";
+        $pdfHTComplaintHousenoFlag = FALSE;
+    }
+    if ( empty ( $zip ) ) {
+        $pdfHTComplaintArray["zipErr"] = "Postcode is vereist.";
+        $pdfHTComplaintZipFlag = FALSE;
+    }
+    // Kijk of complaint leeg is.
+    if ( empty ( $complaint ) ) {
+        $pdfHTComplaintArray["complaintErr"] = "Voer A.U.B. uw klacfHT in.";
+        $pdfHTComplaintComplaintFlag = FALSE;
+    }
+    // Kijk of captchacode leeg is.
+    if ( empty ( $captchaCode ) ) {
+        $pdfHTComplaintArray["captchaErr"] = "Captcha is vereist.";
+        $pdfHTComplaintCaptchaFlag = FALSE;
+    }
+    // Kijk of alles nog op TRUE staat.
+    if ( $pdfHTComplaintFirstnameFlag === TRUE && $pdfHTComplaintSurnameFlag === TRUE && $pdfHTComplaintEmailFlag == TRUE && $pdfHTComplaintTelnoFlag == TRUE 
+            && $pdfHTComplaintStreetFlag === TRUE && $pdfHTComplaintCityFlag = TRUE && $pdfHTComplaintComplaintFlag == TRUE && $pdfHTComplaintCaptchaFlag == TRUE 
+            && $pdfHTComplaintHousenoFlag === TRUE && $pdfHTComplaintZipFlag === TRUE ) {
+        
+        // Zet flags voor validatie email en telefoonnummer. 
+        $pdfHTComplaintArrayEmailIsValidFlag = TRUE;
+        $pdfHTComplaintArrayTelnoIsValidFlag = TRUE;
+        // Kijk of email valid is.
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $pdfHTComplaintArray["emailErr"] = "Email is ongeldig.";
+            $pdfHTComplaintArrayEmailIsValidFlag = FALSE;
+        }
+        // Kijk of telefoonnummer alleen cijfers bevat.
+        if (!is_numeric($telno)) {
+            $pdfHTComplaintArray["telnoErr"] = "Telefoon nummer is ongeldig.";
+            $pdfHTComplaintArrayTelnoIsValidFlag = FALSE;
+        }
+        // Als alles klopt ga door.
+        if ( $pdfHTComplaintArrayEmailIsValidFlag === TRUE && $pdfHTComplaintArrayTelnoIsValidFlag === TRUE ) {
+            // Kijk of captcha fout is.
+            if ($secureImage->check($captchaCode) == FALSE) {
+                $pdfHTComplaintArray["captchaErr"] = "Captcha is fout!";
+            }
+            // Als alles ingevuld is en de captcha goed is maak de pdf.
+            else {
+                class PDF extends FPDF {
+                    function Header() {
+                    $this->Image("lib/fpdf/logo_groot.png",10,5,65);
+                    $this->SetFont("Arial","B",15);
+                    $this->Cell(80);
+                    $this->Cell(35,18,"Meldingformulier",0,0,"C");
+                    $this->Line(20, 35, 210-20, 35);
+                    $this->Ln(30);
+                    }
+                }
+                $pdf = new PDF();
+                $pdf->AliasNbPages();
+                $pdf->SetLeftMargin(10);
+                $pdf->AddPage();
+                
+                $pdf->SetFont("Arial","",10);
+                $pdf->SetLeftMargin(10);
+                
+                $pdf->SetXY(23, 40);
+                $pdf->Cell(35,10,"Voornaam: ");
+                $pdf->SetXY(60, 40);
+                $pdf->Cell(35,10,$firstname);
+                
+                $pdf->SetXY(23, 45);
+                if (empty($insertion)) {
+                    $pdf->Cell(35,10,"AcfHTernaam: ");
+                    $pdf->SetXY(60, 45);
+                    $pdf->Cell(35,10,$surname);
+                }
+                else {
+                    $pdf->Cell(35,10,"AcfHTernaam: ");
+                    $pdf->SetXY(60, 45);
+                    $pdf->Cell(35,10,$surname . ", " . $insertion);
+                }
+                
+                $pdf->SetXY(23, 50);
+                $pdf->Cell(35,10,"Email: ");
+                $pdf->SetXY(60, 50);
+                $pdf->Cell(35,10,$email);
+                
+                $pdf->SetXY(23, 55);
+                $pdf->Cell(35,10,"Telefoonnummer: ");
+                $pdf->SetXY(60, 55);
+                $pdf->Cell(35,10,$telno);
+                
+                $pdf->SetXY(23, 60);
+                $pdf->Cell(35,10,"Woonplaats: ");
+                $pdf->SetXY(60, 60);
+                $pdf->Cell(35,10,$city);
+                
+                $pdf->SetXY(23, 65);
+                $pdf->Cell(35,10,"Adres: ");
+                $pdf->SetXY(60, 65);
+                $pdf->Cell(35,10,$street . " " . $houseno);
+                
+                $pdf->SetXY(23, 70);
+                $pdf->Cell(35,10,"Postcode: ");
+                $pdf->SetXY(60, 70);
+                $pdf->Cell(35,10,$zip);
+                
+                $pdf->SetXY(23, 75);
+                $pdf->Line(20, 85, 210-20, 85);
+                $pdf->SetXY(23, 90);
+                $pdf->Multicell(165,4.5,"Melding: \n" . $complaint);
+                
+                $pdfHTComplaintArray["pdf"] = $pdf;
+                $pdfHTComplaintArray["result"] = TRUE;
+            }
+        }
+    }
+    return $pdfHTComplaintArray;
+}
+
+
+        ?>
+    </body>
+</HTml>
