195aba27e8778c839ea49d33dc2c28e788644e78
Koen <koenwillemse1@gmail.com>
2017-12-14 12:23:21 +0100

    Kleine fixes met upload systeem

--- lib/upload/upload.php
+++ lib/upload/upload.php
@@ -8,26 +8,44 @@ require $_SERVER['DOCUMENT_ROOT'] . "/lib/account/sql.php";
 function uploadFile()
 {
     try {
+        //Get the next availabe id
         $id = getId();
 
         if (isset($_POST['submit'])) {
+
+            //Allowed extentions
+            $EXTENTIONS = array("jpg", "jpeg", "png", "gif");
+
             if (count($_FILES['upload']['name']) > 0) {
+
                 //Loop through each file
                 for ($i = 0; $i < count($_FILES['upload']['name']); $i++) {
+
                     //Get the temp file path
                     $tmpFilePath = $_FILES['upload']['tmp_name'][$i];
+
+                    //Check if the file is an image
+                    $ext = pathinfo($_FILES['upload']['name'][$i], PATHINFO_EXTENSION);
+                    if (!in_array($ext, $EXTENTIONS)) {
+                        throw new RuntimeException("Alleen foto's zijn toegestaan!");
+                    }
 
                     //Make sure we have a filepath
                     if ($tmpFilePath != "") {
 
                         //save the url and the file
-                        $filePath = $_SERVER['DOCUMENT_ROOT'] . "/uploads/" . date('d-m-Y-H-i-s') . '-' . $_FILES['upload']['name'][$i];
+                        $filePath =
+                            $_SERVER['DOCUMENT_ROOT'] . "/uploads/" .
+                            date('d-m-Y-H-i-s') . '-' .
+                            random_string(8) . '-' .
+                            $_FILES['upload']['name'][$i];
 
                         //Upload the file into the temp dir
                         if (move_uploaded_file($tmpFilePath, $filePath)) {
 
                             $files[] = $filePath;
 
+                            //Insert the file into the database
                             insertPictureInDB(str_replace($_SERVER['DOCUMENT_ROOT'] . "/uploads/", "", $filePath), $id);
                         }
                     }
@@ -37,7 +55,7 @@ function uploadFile()
             return $id;
         }
 
-        return false;
+        throw new RuntimeException("Form niet gesubmit, probeer opnieuw!");
 
     } catch (RuntimeException $ex) {
         global $UPLOAD_ERROR;
@@ -75,6 +93,18 @@ function getPictures($id)
     $statement->execute(array($id));
 
     return $statement->fetchAll();
+}
+
+function random_string($length)
+{
+    $key = '';
+    $keys = array_merge(range(0, 9), range('a', 'z'));
+
+    for ($i = 0; $i < $length; $i++) {
+        $key .= $keys[array_rand($keys)];
+    }
+
+    return $key;
 }
 
 ?>
