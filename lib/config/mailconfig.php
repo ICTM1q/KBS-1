ac080afc202e44fd26ec262cffa78789b54b756a
Remco Stoelwinder <remcostoelwinder@hotmail.com>
2017-12-14 10:14:42 +0100

    Update mailconfig.php

--- lib/config/mailconfig.php
+++ lib/config/mailconfig.php
@@ -2,5 +2,5 @@
 
 //  Config file voor variabelen voor mail login info.
 
-$emailaccount = "remcostoelwinder42@gmail.com";
-$emailpassword = "vzxazthwvlvgwnco";
+$emailaccount = "";
+$emailpassword = "";
