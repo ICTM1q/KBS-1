e141c55e7e935d9d441a6c242698825552c8a73b
Tristan Willems <tristan_j_w@hotmail.com>
2017-12-19 08:59:17 +0100

    Removed redundant duplicate code

--- lib/mail/sql.php
+++ lib/mail/sql.php
@@ -1,36 +1,25 @@
 <?php
+if (!isset($SQL_AVAILABLE)) {
+    require $_SERVER['DOCUMENT_ROOT'] . "/lib/account/sql.php";
+}
 
 function getFormEmails ( $type ) {
+    $conn = new PDO ( "mysql:host=localhost;dbname=$dbname;", $user, $dbpassword);
     if ( $type === "Contact" ) {
-        include $_SERVER['DOCUMENT_ROOT']."/lib/config/sqlconfig.php";
-        $conn = new PDO ( "mysql:host=localhost;dbname=$dbname;", $user, $dbpassword);
         $stmt = $conn->prepare ( "SELECT email FROM receiver WHERE contact = 1" );
-        $stmt->execute ( array ( ) );
-        $row = $stmt->fetchAll();
-        return $row;
     }
     if ( $type === "Taxation" ) {
-        include $_SERVER['DOCUMENT_ROOT']."/lib/config/sqlconfig.php";
-        $conn = new PDO ( "mysql:host=localhost;dbname=$dbname;", $user, $dbpassword);
         $stmt = $conn->prepare ( "SELECT email FROM receiver WHERE taxation = 1" );
-        $stmt->execute ( array ( ) );
-        $row = $stmt->fetchAll();
-        return $row;
     }
     if ( $type === "Melding" ) {
-        include $_SERVER['DOCUMENT_ROOT']."/lib/config/sqlconfig.php";
-        $conn = new PDO ( "mysql:host=localhost;dbname=$dbname;", $user, $dbpassword);
         $stmt = $conn->prepare ( "SELECT email FROM receiver WHERE complaint = 1" );
-        $stmt->execute ( array ( ) );
-        $row = $stmt->fetchAll();
-        return $row;
     }
+    $stmt->execute ( array ( ) );
+    $row = $stmt->fetchAll();
+    return $row;
 }
 
 function insertIntoMaillist ( $email ) {
-    if (!isset($SQL_AVAILABLE)) {
-        require $_SERVER['DOCUMENT_ROOT'] . "/lib/account/sql.php";
-    }
     $token = randString(10);
     $conn = connectToDatabase();
     $stmt = $conn->prepare ( "INSERT INTO mail_list ( email, token ) VALUES ( ?, ? )" );
@@ -38,9 +27,6 @@ function insertIntoMaillist ( $email ) {
 }
 
 function getAllMallist ( ) {
-    if (!isset($SQL_AVAILABLE)) {
-        require $_SERVER['DOCUMENT_ROOT'] . "/lib/account/sql.php";
-    }
     $conn = connectToDatabase();
     $stmt = $conn->prepare ( "SELECT * FROM mail_list" );
     $stmt->execute();
@@ -56,9 +42,6 @@ function getMaillistToken ( $conn, $email) {
 }
 
 function unsubscribe ( $token, $email ) {
-    if (!isset($SQL_AVAILABLE)) {
-        require $_SERVER['DOCUMENT_ROOT'] . "/lib/account/sql.php";
-    }
     $conn = connectToDatabase();
     $checkToken = getMaillistToken($conn, $email);
     if ( $token === $checkToken[0][0] ) {
