96629bf9541a4876ef889391122820fe0745e4fa
Jousef <jouseftaha1298@gmail.com>
2017-12-21 08:43:44 +0100

    Taxatie site

--- taxatiesite/onskantoor.php
+++ taxatiesite/onskantoor.php
@@ -6,7 +6,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Taxatie</title>
+    <title>Hoksbergen Makelaardij - Ons Kantoor</title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
     <link href="css/style.css" rel="stylesheet">
