16cb70b50151a3c44325c114ddf44f354e5cfe2e
Koen <koenwillemse1@gmail.com>
2017-12-21 10:04:55 +0100

    Revert "Merge"
    This reverts commit c8ac9de417abcfad6c6770d8dbf3ecfd12c52f5b, reversing
    changes made to d4ae25c0427a0695e2d620d7448a5f73bfbbffb7.

--- aanbod.php
+++ aanbod.php
@@ -60,7 +60,7 @@
             <div id="pand-<?= $residence['pandid'] ?>" class="pand">
                 <div class="row">
                     <div class="col-md-4">
-                        <img src="<?= $pictures == null ? "https://via.placeholder.com/350x260" : "uploads/" . $pictures->fetch_array()[$PICTURE_PATH] ?>"
+                        <img src="<?= $pictures == null ? "https://via.placeholder.com/350x260" : "uploads/" . $pictures->fetch_array()['path'] ?>"
                              class="w-100  pand-pic">
                     </div>
                     <div class="col-md-8 px-3">
--- navbar.php
+++ navbar.php
@@ -5,63 +5,40 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <html>
-<head>
-    <title>Huur en beheer</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css"
-          integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous"
-          rel="stylesheet">
-    <link href="css/huurenbeheer.css" rel="stylesheet">
-    <link href="css/navbar.css" rel="stylesheet">
-    <script defer src="https://use.fontawesome.com/releases/v5.0.1/js/all.js"></script>
-
-    <?php include_once "lib/vars.php" ?>
-</head>
-
-<body>
-<nav class="navbar navbar-expand-lg navbar-custom fixed-top">
-
-    <div class="container">
-
-        <a class="navbar-brand" href="index.php">
-
-            <img src="css/img/logo_groot.png" width="214" height="90" class="d-inline-block align-top nav-logo" alt=""></a>
-
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
-                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon fa fa-bars"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-
-            <ul class="navbar-nav ml-auto">
-                <li>
-                    <a class="nav-link <?php if ($_SERVER[$GLOBAL_PHP_SELF] == $HEADER_HOME) {
-                        print "active";
-                    }; ?>" href="home.php">Home</a>
-                </li>
-                <li>
-                    <a class="nav-link <?php if ($_SERVER[$GLOBAL_PHP_SELF] == $HEADER_KANTOOR) {
-                        echo "active";
-                    }; ?>" href="kantoor.php">Kantoor</a>
-                </li>
-                <li>
-                    <a class="nav-link <?php if ($_SERVER[$GLOBAL_PHP_SELF] == $HEADER_AANBOD || $_SERVER[$GLOBAL_PHP_SELF] == $HEADER_WONING) {
-                        echo "active";
-                    }; ?>" href="aanbod.php">Aanbod</a>
-                </li>
-                <li>
-                    <a class="nav-link <?php if ($_SERVER[$GLOBAL_PHP_SELF] == $HEADER_VOORWAARDEN) {
-                        echo "active";
-                    }; ?>" href="huurvoorwaarden.php">Huurvoorwaarden</a>
-                </li>
-                <li>
-                    <a class="nav-link <?php if ($_SERVER[$GLOBAL_PHP_SELF] == $HEADER_FORMS || $_SERVER[$GLOBAL_PHP_SELF] == $HEADER_MELDING || $_SERVER[$GLOBAL_PHP_SELF] == $HEADER_CONTACT) {
-                        echo "active";
-                    }; ?>" href="formulieren.php">Formulieren</a>
-                </li>
-            </ul>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+        <link href="css/navbar.css" rel="stylesheet">
+        <link href="css/huurenbeheer.css" rel="stylesheet">
+        <script defer src="https://use.fontawesome.com/releases/v5.0.1/js/all.js"></script>
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-custom fixed-top">
+     <div class="container">
+       <a class="navbar-brand" href="index.php">
+           <img src="css/img/logo_groot.png" width="214" height="90" class="d-inline-block align-top" alt=""></a>
+       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+         <span class="navbar-toggler-icon fa fa-bars"></span>
+       </button>
+       <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li>
+                <a class="nav-link <?php if ($_SERVER['PHP_SELF'] == "index.php"){echo "active";};?>" href="home.php">Home</a>
+            </li>
+            <li>
+                <a class="nav-link <?php if ($_SERVER['PHP_SELF'] == "kantoor.php"){echo "active";};?>" href="kantoor.php">Kantoor</a>
+            </li>
+            <li>
+                <a class="nav-link <?php if ($_SERVER['PHP_SELF'] == "aanod.php"){echo "active";};?>" href="aanbod.php">Aanbod</a>
+            </li>
+            <li>
+                <a class="nav-link <?php if ($_SERVER['PHP_SELF'] == "huurvoorwaarden.php"){echo "active";};?>" href="huurvoorwaarden.php">Huurvoorwaarden</a>
+            </li>
+            <li>
+                <a class="nav-link <?php if ($_SERVER['PHP_SELF'] == "formulieren.php"){echo "active";};?>" href="formulieren.php">Formulieren</a>
+            </li>
+          </ul>
         </div>
       </div>
     </nav>
